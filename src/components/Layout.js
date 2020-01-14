066c941aa34d01d9b9b46fd7bf53542028a08d56
Sean DMR <sean@flyinggrizzly.net>
2020-01-14 11:39:58 +0000

    Update footer

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -64,9 +64,7 @@ class Layout extends React.Component {
         {header}
         {children}
         <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
+          © 2019-{new Date().getFullYear()}, Scrumpy Games/Sean DMR
         </footer>
       </div>
     )
