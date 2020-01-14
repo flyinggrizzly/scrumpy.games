995d8484084a9254d6af072078a6cb83f7fae328
Sean DMR <sean@flyinggrizzly.net>
2020-01-14 13:51:49 +0000

    Add Nav to layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,72 +1,42 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import { Container, Navbar, Nav } from 'react-bootstrap'
 
 import { rhythm, scale } from '../utils/typography'
 
 class Layout extends React.Component {
   render() {
-    const { location, title, children } = this.props
+    const { location, title, children, twitter, eventbriteProfile, mailingList } = this.props
     const rootPath = `${__PATH_PREFIX__}/`
-    let header
 
-    if (location.pathname === rootPath) {
-      header = (
-        <h1
-          style={{
-            ...scale(1.5),
-            marginBottom: rhythm(1.5),
-            marginTop: 0,
-          }}
-        >
-          <Link
-            style={{
-              boxShadow: `none`,
-              textDecoration: `none`,
-              color: `inherit`,
-            }}
-            to={`/`}
-          >
-            {title}
-          </Link>
-        </h1>
-      )
-    } else {
-      header = (
-        <h3
-          style={{
-            fontFamily: `Montserrat, sans-serif`,
-            marginTop: 0,
-            marginBottom: rhythm(-1),
-          }}
-        >
-          <Link
-            style={{
-              boxShadow: `none`,
-              textDecoration: `none`,
-              color: `inherit`,
-            }}
-            to={`/`}
-          >
-            {title}
-          </Link>
-        </h3>
-      )
-    }
     return (
-      <div
-        style={{
-          marginLeft: `auto`,
-          marginRight: `auto`,
-          maxWidth: rhythm(24),
-          padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
-        }}
-      >
-        {header}
-        {children}
-        <footer>
-          © 2019-{new Date().getFullYear()}, Scrumpy Games/Sean DMR
-        </footer>
-      </div>
+      <React.Fragment>
+        <Navbar>
+          <Navbar.Brand style={{ fontSize: '2.4em' }}>
+            Scrumpy Games
+          </Navbar.Brand>
+          <Container className='justify-content-end' fluid>
+            <Nav>
+              <Nav.Item><Nav.Link href={ eventbriteProfile }>Events</Nav.Link></Nav.Item>
+              <Nav.Item><Nav.Link href={ mailingList }>Mailing list</Nav.Link></Nav.Item>
+              <Nav.Item><Nav.Link href={ twitter }>Twitter</Nav.Link></Nav.Item>
+            </Nav>
+          </Container>
+        </Navbar>
+        <div
+          style={{
+            marginLeft: `auto`,
+            marginRight: `auto`,
+            maxWidth: rhythm(24),
+            padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
+          }}
+        >
+          {children}
+          <footer>
+            © 2019-{new Date().getFullYear()}, Scrumpy Games/Sean DMR
+          </footer>
+        </div>
+      </React.Fragment>
     )
   }
 }
