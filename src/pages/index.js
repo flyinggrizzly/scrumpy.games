a7a84bebb0b30a0ff6b076479cb8dfc2d0970ff9
Sean DMR <sean@flyinggrizzly.net>
2020-03-08 13:11:24 +0000

    Add image and clean up index styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,43 +21,55 @@ const Index = ({ data, location }) => {
         keywords={[ `roleplaying-game`, `rpg`, `Frome`, `Southwest UK`, `tabletop games`, `ttrpg`, `trpg`, `LARP` ]}
       />
 
-      <h2>
-        Welcome to Scrumpy Games!
-      </h2>
-      <p>
-        We are a group of roleplayers in the Southwest of England, mostly centered in/around Frome.
-      </p>
-      <p>
-        We are open to everyone, regardless of skill-level/experience with roleplaying games. All that we require
-        of participants is that everyone be welcoming to everyone else and that we all work together to create a safe
-        and fun environment for everyone.
-      </p>
-      <p>
-        You can keep up with our events on by <a href={ mailingList }>joining our mailing list</a>, or keep an eye on <a href={ eventbriteProfile }>our Eventbrite profile</a>, or on our (nascent...) <a href={ `https://twitter.com/${ twitter }` }>Twitter</a>.
-      </p>
+      <Section>
+        <h2>
+          Welcome to Scrumpy Games!
+        </h2>
+        <p>
+          We are a group of roleplayers in the Southwest of England, mostly centered in/around Frome.
+        </p>
+        <p>
+          We are open to everyone, regardless of skill-level/experience with roleplaying games. All that we require
+          of participants is that everyone be welcoming to everyone else and that we all work together to create a safe
+          and fun environment for everyone.
+        </p>
+        <p>
+          You can keep up with our events on by <a href={ mailingList }>joining our mailing list</a>, or keep an eye on <a href={ eventbriteProfile }>our Eventbrite profile</a>, or on our (nascent...) <a href={ `https://twitter.com/${ twitter }` }>Twitter</a>.
+        </p>
+      </Section>
 
-      <h3>
-        Single Session Saturdays
-      </h3>
-      <p>
-        We regularly run <strong>Single Session Saturdays</strong> (about once a month) to meet new people and try out new games.
-        We've played <a href="https://bullypulpitgames.com/games/fiasco/"><i>Fiasco</i></a> by Jason
-        Morningstar, <a href="https://gshowitt.itch.io/honey-heist"><i>Honey Heist</i></a> by Grant Howitt, and <a href="https://bladesinthedark.com/"><i>Blades in the Dark</i></a> by John Harper to name a few!
-      </p>
-      <p>
-        Our next Saturday event will be on <strong>Saturday February 8 in Frome</strong>, and we'd <a href="https://www.eventbrite.com/e/single-session-games-at-the-griffin-tickets-85672391429">love for you to join us!</a>
-      </p>
+      <Section>
+        <h3>
+          Ongoing Drop-in Games
+        </h3>
+        <img src="/event-images/masks.jpg" alt="Cover image for Masks: A New Generation, showing young teenage superheroes of different ethnicities, genders, backgrounds, and with different superpowers, representing the different game playbooks." />
+        <p>
+          We also run (roughly) monthly games with a rotating cast of characters. Currently we are playing <a href="https://www.magpiegames.com/masks/"><i>Masks: A New Generation</i></a> by Brendan Conway.
+        </p>
+      </Section>
 
-      <h3>
-        Ongoing Drop-in Games
-      </h3>
-      <p>
-        We also run (roughly) monthly games with a rotating cast of characters. Currently we are playing <a href="https://www.magpiegames.com/masks/"><i>Masks: A New Generation</i></a> by Brendan Conway.
-      </p>
-
+      <Section>
+        <h3>
+          Single Session Saturdays
+        </h3>
+        <p>
+          We regularly run <strong>Single Session Saturdays</strong> (about once a month) to meet new people and try out new games.
+          We've played <a href="https://bullypulpitgames.com/games/fiasco/"><i>Fiasco</i></a> by Jason
+          Morningstar, <a href="https://gshowitt.itch.io/honey-heist"><i>Honey Heist</i></a> by Grant Howitt, and <a href="https://bladesinthedark.com/"><i>Blades in the Dark</i></a> by John Harper to name a few!
+        </p>
+        <p>
+          Our next Saturday event will be on <strong>Saturday February 8 in Frome</strong>, and we'd <a href="https://www.eventbrite.com/e/single-session-games-at-the-griffin-tickets-85672391429">love for you to join us!</a>
+        </p>
+    </Section>
     </Layout>
   )
 }
+
+const Section = ({ children }) => (
+  <section style={{ borderBottom: "2px solid #ccc", marginBottom: '2em' }}>
+    { children }
+  </section>
+)
 
 export default Index
 
